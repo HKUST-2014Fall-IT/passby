88b92e4759575f3a5a6fa93b96409e881c0ee023
Cedric Liang <916216792@qq.com>
2014-12-01 12:09:05 +0800

    first v0.1
    first v0.1

---
+++ src/com/passby/SettingPage.java
@@ -0,0 +1,5 @@
+package com.passby;
+
+public class SettingPage {
+
+}
---
+++ src/com/passby/WelcomePage.java
@@ -0,0 +1,8 @@
+package com.passby;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomePage extends Activity {
+
+}
